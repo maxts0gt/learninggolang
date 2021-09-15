601fbd4cbb659dcb8bada45beefad3789d94862d
Max <46038015+maxts0gt@users.noreply.github.com>
2021-09-15 14:27:19 +0900

    Add files via upload

---
+++ jedi_A/exercise_01.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//	c := make(chan int)
+	//
+	//	go func() {
+	//		c <- 42
+	//	}()
+	//	fmt.Println(<-c)
+
+	// Buffered Channel Solutino
+	c := make(chan int, 1)
+
+	c <- 42
+
+	fmt.Println(<-c)
+}
